29914cb5445f8594e3196793df22264e8f432997
conorrific <conorbellchambers@gmail.com>
2018-05-01 12:24:22 -0500

    two numbers added together

---
+++ addnums.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter a whole number of your choice: "))
+num2 = int(input("Enter a whole number of your choice: "))
+new_num = num1 + num2
+print(f"The first number you entered '{num1}' when added to the second number you entered '{num2}' results in a total of {new_num}!")
