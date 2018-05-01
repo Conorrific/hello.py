29a0e987940deb72250dd02bc35974665b5693b0
conorrific <conorbellchambers@gmail.com>
2018-05-01 11:14:47 -0500

    May 1 lesson

--- hello.py
+++ hello.py
@@ -4,15 +4,26 @@ first_name = "John"
 
 last_name = " Doe"
 
-print(first_name + "," + last_name)
+#print(first_name + "," + last_name)
 
-name = input("Enter first name: ")
-print("Hello " + name)
+#name = input("Enter first name: ")
+#print("Hello " + name)
 
 no1 = int(input("Enter first number"))
 no2 = int(input("Enter second number"))
 
-result = no1 + no2
-print(result)
+#result = no1 + no2
+#print(result)
 
-print("The first number is {no1}, second number is {no2} and the result is {no1+no2}")
+print(f"The first number is {no1}, second number is {no2} and the result is {no1+no2}")
+
+#use the above before using the str() method and remember the f
+#result = "The first number is " + str(no1)
+#print(result)
+
+
+
+
+
+
+
