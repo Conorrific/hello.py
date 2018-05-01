ad33827d2d9b52418f24b225598c2961c8664100
conorrific <conorbellchambers@gmail.com>
2018-05-01 12:15:49 -0500

    tip calculator exercise

--- tipcalc.py
+++ tipcalc.py
@@ -1,1 +1,7 @@
-print("some text here") 
+print("Hello World")
+
+subtotal = float(input("Enter the total of the bill after tax: "))
+tip = float(input("Enter the percentage you would like to tip: "))
+tip_percent = round((tip / 100) * subtotal, 2)
+newtotal = round(tip_percent + subtotal, 2)
+print(f"The subtoal of the bill is ${subtotal}, the tip amount is ${tip_percent}, the total amount due is ${newtotal}")
