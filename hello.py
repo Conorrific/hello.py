f02f7245189aea0a24932c47a3aad817423c975f
conorrific <conorbellchambers@gmail.com>
2018-05-01 10:59:07 -0500

    input with print

--- hello.py
+++ hello.py
@@ -5,3 +5,14 @@ first_name = "John"
 last_name = " Doe"
 
 print(first_name + "," + last_name)
+
+name = input("Enter first name: ")
+print("Hello " + name)
+
+no1 = int(input("Enter first number"))
+no2 = int(input("Enter second number"))
+
+result = no1 + no2
+print(result)
+
+print("The first number is {no1}, second number is {no2} and the result is {no1+no2}")
